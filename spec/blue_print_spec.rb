365f25cbad9f80a1909c929cbf74dd9a415fd93c
Sho Kusano <rosylilly@aduca.org>
2014-03-14 15:13:50 +0900

    Add BluePrint spec

---
+++ spec/blue_print_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe BluePrint do
+  let(:env) { double }
+
+  describe '#env' do
+    it 'be inherited by parent thread' do
+      BluePrint.env = env
+
+      env.should_receive(:message).twice
+
+      BluePrint.env.message
+      Thread.new { BluePrint.env.message }.join
+    end
+  end
+
+  describe '#clear!' do
+    it 'be clear only current thread' do
+      BluePrint.env = env
+
+      Thread.new do
+        BluePrint.clear!
+      end.join
+
+      expect(BluePrint.env).to eq(env)
+
+      BluePrint.clear!
+
+      expect(BluePrint.env).to be_nil
+    end
+  end
+end
