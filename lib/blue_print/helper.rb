e21e3fea95be6b65e5124412c39cdd5103155b6f
Sho Kusano <rosylilly@aduca.org>
2014-03-18 23:29:00 +0900

    Implment BluePrint::Helper#without_context_of

--- lib/blue_print/helper.rb
+++ lib/blue_print/helper.rb
@@ -5,9 +5,13 @@ module BluePrint::Helper
     context = BluePrint::Context.resolve(name_or_context)
 
     if context.active?
-      yield BluePrint.env
+      block_given? && yield(BluePrint.env)
     else
-      fallback.respond_to?(:call) && fallback.call
+      fallback.respond_to?(:call) && fallback.call(BluePrint.env)
     end
+  end
+
+  def without_context_of(name_or_context, fallback = nil, &block)
+    within_context_of(name_or_context, block, &fallback)
   end
 end
--- spec/lib/blue_print/helper_spec.rb
+++ spec/lib/blue_print/helper_spec.rb
@@ -35,4 +35,37 @@ describe BluePrint::Helper do
       end
     end
   end
+
+  describe '#without_context_of' do
+    context 'with deactive context' do
+      let(:context) { double(active?: false) }
+
+      it 'runs block' do
+        helper.should_receive(:message).once
+        helper.without_context_of(context) do |env|
+          expect(env).to eq(BluePrint.env)
+          helper.message
+        end
+      end
+    end
+
+    context 'with active context' do
+      let(:context) { double(active?: true) }
+
+      it 'not runs block' do
+        helper.should_not_receive(:message)
+        helper.without_context_of(context) do
+          helper.message
+        end
+      end
+
+      it 'runs fallback' do
+        helper.should_receive(:fallback)
+        helper.should_not_receive(:message)
+        helper.without_context_of(context, proc { helper.fallback }) do
+          helper.message
+        end
+      end
+    end
+  end
 end
